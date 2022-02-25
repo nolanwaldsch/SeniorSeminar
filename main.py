ada129170f5e583bf0863ce2bc2a4373410acfe1
nolanwaldsch <nobewon@gmail.com>
2022-02-25 12:55:27 -0600

    finished
    finished the basic screen capture code.
    Moved some code into a different document.

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import numpy as np
+from time import time
+from ScreenCapture import WindowCapture
+
+
+wincap = WindowCapture('WindowName')
+
+loop_time = time()
+while(True):
+    screenshot = wincap.get_screenshot()
+
+    cv.imshow('Computer Vision', screenshot)
+
+    print('FPS {}'.format(1 / time()-loop_time))
+    loop_time = time()
+
+    #press 'q' with the output window to force exit
+    #waits 1ms every loop to process key presses
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+window_capture()
+print('Done.')
