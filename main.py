349c72b74ca3cb1a1f4094664bc1bef9cfdcca47
nolanwaldsch <nobewon@gmail.com>
2022-04-19 20:50:21 -0500

    Added video recording
    Added video recording file.
    added video recording to program.

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ while(True):
     print('FPS {}'.format(1 / time()-loop_time))
     loop_time = time()
 
+    if cv.waitKey(1) == ord('s'):
+        VideoRecording.RecordVideo
+        break
+
     #press 'q' with the output window to force exit
     #waits 1ms every loop to process key presses
     if cv.waitKey(1) == ord('q'):
